26c5000a8aa95b3baab779c0024a78b4aa6dbc3c
Robert Chen <chen1fan@icloud.com>
2021-10-15 08:39:46 -0400

    fix error

--- Assets/Core/Plugins/iOS/MapboxMobileEvents/include/MapboxMobileEvents/MapboxMobileEvents.h
+++ Assets/Core/Plugins/iOS/MapboxMobileEvents/include/MapboxMobileEvents/MapboxMobileEvents.h
@@ -8,7 +8,3 @@ FOUNDATION_EXPORT const unsigned char MapboxMobileEventsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MapboxMobileEvents/PublicHeader.h>
 
-#import <MapboxMobileEvents/MMEConstants.h>
-#import <MapboxMobileEvents/MMEEvent.h>
-#import <MapboxMobileEvents/MMETypes.h>
-#import <MapboxMobileEvents/MMEEventsManager.h>
